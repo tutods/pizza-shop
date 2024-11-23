1ad31efe564a872f0c78a8b2acd6c5a0c1db14ec
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-11-23 16:35:51 +0000

    feat: sign in page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,30 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { Helmet } from 'react-helmet-async';
 
 function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <h1>Sign In</h1>
+      <div className="p-8">
+        <section className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="font-semibold text-2xl tracking-tight">Acessar Painel</h1>
+            <p className="text-muted-foreground text-sml">Acompanhe suas vendas pelo painel do parceiro!</p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">O seu email</Label>
+              <Input placeholder="Insira o seu email" id="email" type="email" />
+            </div>
+            <Button className="w-full" type="submit">
+              Acessar painel
+            </Button>
+          </form>
+        </section>
+      </div>
     </>
   );
 }
