f5d1f6a46ad9184c2719ca0dd38572b53611da87
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-11-23 10:20:20 +0000

    feat: project cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 
-createRoot(document.getElementById('root')!).render(
+const element = document.getElementById('root') as HTMLElement;
+
+createRoot(element).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
