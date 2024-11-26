b7accbe0ef56dbbca03f9266429808f8bb9193e6
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-11-26 15:57:58 +0000

    chore: rename file

--- apps/frontend/src/routes.tsx
+++ apps/frontend/src/routes.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router';
 import { AppLayout } from '~/pages/_layouts/app';
 import { AuthLayout } from '~/pages/_layouts/auth';
 import { Dashboard } from '~/pages/app/dashboard';
-import { Orders } from '~/pages/app/orders/orders';
+import { Orders } from '~/pages/app/orders';
 import { SignIn } from '~/pages/auth/sign-in';
 import { SignUp } from '~/pages/auth/sign-up';
 
