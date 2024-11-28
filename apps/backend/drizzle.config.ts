19333cedc472748c4792688325f46b32123da83b
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-11-28 14:36:04 +0000

    feat: drizzle scripts

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -5,4 +5,7 @@ export default {
   schema: './src/db/schema/index.ts',
   out: './drizzle',
   dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL ?? '',
+  },
 } satisfies Config;
