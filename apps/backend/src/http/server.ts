359c573f0acf85f1c05ea1c1d5197b51e527ba5f
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-11-28 15:28:11 +0000

    feat: body type

--- apps/backend/src/http/server.ts
+++ apps/backend/src/http/server.ts
@@ -1,9 +1,40 @@
 import chalk from 'chalk';
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
+import { db } from '~/db/connection';
+import { restaurants, users } from '~/db/schema';
 
-const app = new Elysia().get('/', () => {
-  return 'Hello world';
-});
+const app = new Elysia().post(
+  '/restaurants',
+  async ({ body, set }) => {
+    const { restaurantName, managerName, email, phone } = body;
+
+    const [manager] = await db
+      .insert(users)
+      .values({
+        name: managerName,
+        email,
+        phone,
+        role: 'manager',
+      })
+      .returning({
+        id: users.id,
+      });
+    await db.insert(restaurants).values({
+      name: restaurantName,
+      managerId: manager.id,
+    });
+
+    set.status = 204;
+  },
+  {
+    body: t.Object({
+      restaurantName: t.String(),
+      managerName: t.String(),
+      phone: t.String(),
+      email: t.String({ format: 'email' }),
+    }),
+  },
+);
 
 app.listen(3333, () => {
   console.info(chalk.bgGreen('🚀 HTTP server running on port 3333!'));
