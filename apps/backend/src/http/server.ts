baf370b275b58b0a512806373e5a5c472caae693
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-11-29 06:06:55 +0000

    feat: send authentication link

--- apps/backend/src/http/server.ts
+++ apps/backend/src/http/server.ts
@@ -1,40 +1,9 @@
 import chalk from 'chalk';
-import { Elysia, t } from 'elysia';
-import { db } from '~/db/connection';
-import { restaurants, users } from '~/db/schema';
+import { Elysia } from 'elysia';
+import { registerRestaurant } from '~/http/routes/register-restaurant';
+import { sendAuthLink } from '~/http/routes/send-auth-link';
 
-const app = new Elysia().post(
-  '/restaurants',
-  async ({ body, set }) => {
-    const { restaurantName, managerName, email, phone } = body;
-
-    const [manager] = await db
-      .insert(users)
-      .values({
-        name: managerName,
-        email,
-        phone,
-        role: 'manager',
-      })
-      .returning({
-        id: users.id,
-      });
-    await db.insert(restaurants).values({
-      name: restaurantName,
-      managerId: manager.id,
-    });
-
-    set.status = 204;
-  },
-  {
-    body: t.Object({
-      restaurantName: t.String(),
-      managerName: t.String(),
-      phone: t.String(),
-      email: t.String({ format: 'email' }),
-    }),
-  },
-);
+const app = new Elysia().use(sendAuthLink).use(registerRestaurant);
 
 app.listen(3333, () => {
   console.info(chalk.bgGreen('🚀 HTTP server running on port 3333!'));
