c34f4ca1326ec6a99ae9a58424e5a3e32f028f52
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-12-03 20:47:18 +0000

    fix: gitignore

--- apps/backend/src/http/server.ts
+++ apps/backend/src/http/server.ts
@@ -1,9 +1,24 @@
+import cookie from '@elysiajs/cookie';
+import jwt from '@elysiajs/jwt';
 import chalk from 'chalk';
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
+import { env } from '~/env';
 import { registerRestaurant } from '~/http/routes/register-restaurant';
 import { sendAuthLink } from '~/http/routes/send-auth-link';
 
-const app = new Elysia().use(sendAuthLink).use(registerRestaurant);
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
+  .use(sendAuthLink)
+  .use(registerRestaurant);
 
 app.listen(3333, () => {
   console.info(chalk.bgGreen('🚀 HTTP server running on port 3333!'));
