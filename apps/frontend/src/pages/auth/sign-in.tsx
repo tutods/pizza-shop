804e7584f8f30f275355da58cb54c1a0dfd87a84
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-12-19 09:52:02 +0000

    feat: authenticate

--- apps/frontend/src/pages/auth/sign-in.tsx
+++ apps/frontend/src/pages/auth/sign-in.tsx
@@ -1,9 +1,11 @@
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
 import { Helmet } from 'react-helmet-async';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router';
 import { toast } from 'sonner';
 import { z } from 'zod';
+import { signIn } from '~/api/sign-in.ts';
 import { Button } from '~/components/ui/button';
 import { Input } from '~/components/ui/input';
 import { Label } from '~/components/ui/label';
@@ -27,19 +29,23 @@ function SignIn() {
     resolver: zodResolver(signInForm),
   });
 
-  async function handleSignIn(data: SignInForm) {
-    try {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-
+  const { mutateAsync: authenticate } = useMutation<void, Error, SignInForm>({
+    mutationFn: signIn,
+    onSuccess: () => {
       toast.success('Enviamos um link de autenticação para o seu e-mail.', {
-        action: {
-          label: 'Reenviar',
-          onClick: () => handleSignIn(data),
-        },
+        // action: {
+        //   label: 'Reenviar',
+        //   onClick: () => handleSignIn(data),
+        // },
       });
-    } catch {
+    },
+    onError: () => {
       toast.error('Credenciais inválidas.');
-    }
+    },
+  });
+
+  async function handleSignIn(data: SignInForm) {
+    await authenticate(data);
   }
 
   return (
