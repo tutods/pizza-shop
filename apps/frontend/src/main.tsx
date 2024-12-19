b3d838a6ad973a29e381ae0657292b256fa4db65
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-12-19 09:34:08 +0000

    feat: api setup

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -4,8 +4,10 @@ import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router';
 import { router } from '~/routes';
 import '~/index.css';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'sonner';
 import { ThemeProvider } from '~/components/theme/theme-provider';
+import { queryClient } from '~/lib/react-query.ts';
 
 const element = document.getElementById('root') as HTMLElement;
 
@@ -15,7 +17,10 @@ createRoot(element).render(
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors closeButton />
-        <RouterProvider router={router} />
+
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   </StrictMode>,
