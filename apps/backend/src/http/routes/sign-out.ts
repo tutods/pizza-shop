61f31fa01696740dc7cf3656dd3f45f030b166b0
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-12-21 04:13:29 +0000

    fix: sign out

--- apps/backend/src/http/routes/sign-out.ts
+++ apps/backend/src/http/routes/sign-out.ts
@@ -1,8 +1,8 @@
 import { Elysia } from 'elysia';
 import { auth } from '~/http/auth.ts';
 
-const signOut = new Elysia().use(auth).post('/sign-out', async ({ singOut: internalSignOut }) => {
-  internalSignOut('auth');
+const signOut = new Elysia().use(auth).post('/sign-out', async ({ signOut: internalSignOut }) => {
+  internalSignOut();
 });
 
 export { signOut };
